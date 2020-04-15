747a057d35cd72dad7fc6029b8e4b1e1d2e6a207
zhangwen <zhangwen1@unionpay.com>
2020-04-15 14:42:23 +0800

    Add Root Controller
    Signed-off-by: zhangwen1@unionpay.com

---
+++ crane/root.py
@@ -0,0 +1,18 @@
+from pecan import expose, redirect
+from webob.exc import status_map
+from oslo_log import log as logging
+from pecan import rest
+import pecan
+
+LOG = logging.get_loggers(__name__)
+
+class V1Controller(rest.RestController):
+
+    @pecan.expose()
+    def get(self):
+        pecan.response.status = 200
+        return "This is V1 Controller Get.\n"
+    @pecan.expose()
+    def post(self):
+        pecan.response.status = 201
+        return "This is V1 Controller Post.\n"
