7cd76412320c10020a4ec8418311d73633dadfaf
zhangwen <zhangwen1@unionpay.com>
2020-04-20 15:16:12 +0800

    Add SubController.
    Signed-off-by: zhangwen1@unionpay.com

--- crane/root.py
+++ crane/root.py
@@ -6,13 +6,34 @@ import pecan
 
 LOG = logging.getLogger(__name__)
 
-class V1Controller(rest.RestController):
-
-    @pecan.expose()
+class MetricController(rest.RestController):
+    @pecan.expose('json')
     def get(self):
         pecan.response.status = 200
-        return "This is V1 Controller Get.\n"
-    @pecan.expose()
+        return {'cpu':0,'mem':0}
+    @pecan.expose('json')
     def post(self):
         pecan.response.status = 201
-        return "This is V1 Controller Post.\n"
+        return {'cpu':10,'mem':30}
+
+class V1Controller(rest.RestController):
+    
+    def __init__(self):
+        self.sub_controllers = {
+            'metric': MetricController()
+        }
+        for name, ctrl in self.sub_controllers.items():
+            setattr(self, name, ctrl)
+    @pecan.expose('json')
+    def index(self):
+        return {
+            "version": "1.0",
+            "links": [
+                {"rel": "self",
+                 "href": pecan.request.application_url}
+            ] + [
+                {"rel": name,
+                 "href": pecan.request.application_url + "/" + name}
+                for name in sorted(self.sub_controllers)
+            ]
+        }
